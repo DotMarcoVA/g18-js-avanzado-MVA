fe4b17f4fe2e663333efc64a1b46d62013918c87
Marco Valdez <antem.marco@gmail.com>
2022-09-20 19:44:27 -0500

    primera mitad 2009

--- 1. Node/1.6 Modules/1.6.1-common-js/functions.js
+++ 1. Node/1.6 Modules/1.6.1-common-js/functions.js
@@ -14,3 +14,9 @@ function divide(a, b) {
     return a / b;
 }
 
+module.exports = {
+    sum,
+    substract,
+    multiply,
+    divide,
+};
--- 1. Node/1.6 Modules/1.6.1-common-js/index.js
+++ 1. Node/1.6 Modules/1.6.1-common-js/index.js
@@ -1,19 +1,18 @@
-
+const fn = require("./functions");
 
 function main() {
     let numero1 = 10;
     let numero2 = 40;
 
-    const resultSum = sum(numero1, numero2);
-    const resultSubs = substract(numero1, numero2);
-    const resultMult = multiply(numero1, numero2);
-    const resultDiv = divideNumbers(numero1, numero2);
+    const resultSum = fn.sum(numero1, numero2);
+    const resultSubs = fn.substract(numero1, numero2);
+    const resultMult = fn.multiply(numero1, numero2);
+    const resultDiv = fn.divide(numero1, numero2);
 
-    console.log('suma: ', resultSum);
-    console.log('resta: ', resultSubs);
-    console.log('multiplicacion: ', resultMult);
-    console.log('division: ', resultDiv);
-
+    console.log("suma: ", resultSum);
+    console.log("resta: ", resultSubs);
+    console.log("multiplicacion: ", resultMult);
+    console.log("division: ", resultDiv);
 }
 
 main();
